c4cc77ab29b4547aa75578206f30dc62abd79375
Sema Checherinda <Sema.Checherinda@clickhouse.com>
2024-04-29 20:10:16 +0200

    add clickhouse specific options to azure client

--- sdk/storage/azure-storage-blobs/src/blob_container_client.cpp
+++ sdk/storage/azure-storage-blobs/src/blob_container_client.cpp
@@ -111,6 +111,8 @@ namespace Azure { namespace Storage { namespace Blobs {
         _detail::PackageVersion::ToString(),
         std::move(perRetryPolicies),
         std::move(perOperationPolicies));
+
+    m_clickhouseOptions = options.ClickhouseOptions;
   }
 
   BlobClient BlobContainerClient::GetBlobClient(const std::string& blobName) const
@@ -388,4 +390,13 @@ namespace Azure { namespace Storage { namespace Blobs {
         std::move(blockBlobClient), std::move(response.RawResponse));
   }
 
+  const ClickhouseClientOptions & BlobContainerClient::GetClickhouseOptions() const
+  {
+    return m_clickhouseOptions;
+  }
+
+  void BlobContainerClient::SetClickhouseOptions(ClickhouseClientOptions options)
+  {
+    m_clickhouseOptions = std::move(options);
+  }
 }}} // namespace Azure::Storage::Blobs
